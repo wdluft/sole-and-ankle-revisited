c0b4a29879d99b783367443c3ce39515a18b908a
Will Luft <william.d.luft@gmail.com>
2021-09-04 09:33:55 -0400

    Finished mobile menu styles

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
 
-import { QUERIES } from '../../constants';
+import { COLORS, QUERIES } from '../../constants';
 
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
@@ -17,19 +17,19 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
           <VisuallyHidden>Close</VisuallyHidden>
           <Icon id='close' strokeWidth={1} />
         </UnstyledButton>
-        <nav>
-          <a href='/sale'>Sale</a>
-          <a href='/new'>New&nbsp;Releases</a>
-          <a href='/men'>Men</a>
-          <a href='/women'>Women</a>
-          <a href='/kids'>Kids</a>
-          <a href='/collections'>Collections</a>
-        </nav>
-        <footer>
-          <a href='/terms'>Terms and Conditions</a>
-          <a href='/privacy'>Privacy Policy</a>
-          <a href='/contact'>Contact Us</a>
-        </footer>
+        <Nav>
+          <NavLink href='/sale'>Sale</NavLink>
+          <NavLink href='/new'>New&nbsp;Releases</NavLink>
+          <NavLink href='/men'>Men</NavLink>
+          <NavLink href='/women'>Women</NavLink>
+          <NavLink href='/kids'>Kids</NavLink>
+          <NavLink href='/collections'>Collections</NavLink>
+        </Nav>
+        <Footer>
+          <FooterLink href='/terms'>Terms and Conditions</FooterLink>
+          <FooterLink href='/privacy'>Privacy Policy</FooterLink>
+          <FooterLink href='/contact'>Contact Us</FooterLink>
+        </Footer>
       </Content>
     </Overlay>
   );
@@ -54,6 +54,37 @@ const Content = styled(DialogContent)`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  padding: 32px 22px 32px 32px;
+
+  & > button {
+    align-self: flex-end;
+  }
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+`;
+
+const NavLink = styled.a`
+  font-size: 1.125rem;
+  font-weight: 600;
+  text-decoration: none;
+  color: ${COLORS.gray[900]};
+  text-transform: uppercase;
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`;
+
+const FooterLink = styled.a`
+  text-decoration: none;
+  font-size: 0.875rem;
+  color: ${COLORS.gray[700]};
 `;
 
 export default MobileMenu;
